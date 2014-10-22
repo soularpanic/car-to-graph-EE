bde1962c2207ff67bb39fefa04ccc4657850a8a8
Joshua Kossoff <kossoff.j@gmail.com>
2014-10-22 17:48:12 -0400

    Initial commit - placeholder admin controller

---
+++ app/code/local/Soularpanic/CarToGraphEE/controllers/Admin/CartographeeController.php
@@ -0,0 +1,10 @@
+<?php
+class Soularpanic_CarToGraphEE_Admin_CartographeeController
+    extends Mage_Adminhtml_Controller_Action {
+
+    public function indexAction() {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
