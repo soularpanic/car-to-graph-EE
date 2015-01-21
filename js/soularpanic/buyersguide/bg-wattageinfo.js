5616bf5b548d40f64498bd437756b4dc280315a3
Joshua Kossoff <kossoff.j@gmail.com>
2015-01-21 17:02:46 -0500

    Merge branch 'master' into buyersguide_bundle_work
    Conflicts:
    	app/design/frontend/trs_default/default/template/bundle/catalog/product/view/type/bundle/option/select.phtml
    	skin/frontend/trs_default/default/css/base.css
    	skin/frontend/trs_default/default/css/layout-home.css
    	skin/frontend/trs_default/default/css/layout-products.css

--- js/soularpanic/buyersguide/bg-wattageinfo.js
+++
@@ -1,6 +0,0 @@
-var WattageInfo = Class.create({
-   initialize: function(args) {
-       var _args = args || {};
-       console.log('wattage info starting up');
-   }
-});
--- js/soularpanic/buyersguide/bg-wattageinfoinit.js
+++ js/soularpanic/buyersguide/bg-wattageinfoinit.js
@@ -1,3 +1,13 @@
 document.observe('dom:loaded', function() {
-    trsWattagePopup = new WattageInfo();
+    jQuery('.wattageInfoButton').fancybox({
+        autoSize: true,
+        autoCenter: true,
+        minWidth: 475,
+        maxWidth: 500,
+        minHeight: 290,
+        maxHeight: 500,
+        width: '80%',
+        height: '70%',
+        padding: 5
+    });
 });
