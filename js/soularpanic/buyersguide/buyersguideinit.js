b3865071091619ed3b15529844811c456714f439
Rob Shimits | reshimits <r.e@shimits.com>
2015-02-24 16:14:50 -0500

    Mobile Buyers Guide Fix / Buyers Guide Intro (in correlation with mag_trs_design push)

--- js/soularpanic/buyersguide/buyersguideinit.js
+++ js/soularpanic/buyersguide/buyersguideinit.js
@@ -2,4 +2,10 @@ document.observe('dom:loaded', function() {
    trsBuyersGuide = new BuyersGuideController({
         updateCarInputsUrl: $('buyersGuideControllerData').readAttribute('data-carupdateUrl')
    });
+   jQuery( ".buyersGuide-mobileStart" ).click(function() {
+        jQuery(".buyersGuide").toggleClass( "mobile-active" );
+        jQuery(".buyersGuide-mobileStart").toggleClass( "mobile-active" );
+	});
 });
+
+
