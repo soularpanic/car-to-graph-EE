ed16772b490d59de9f9ba7f532fbacefa3e28ea6
Joshua Kossoff <kossoff.j@gmail.com>
2015-06-12 15:48:10 -0400

    Added logic to accomodate H11A v H11B bulbs, a special case for Low Beam HID Systems.

--- app/code/local/Soularpanic/CarToGraphEE/Model/Resource/Buyersguide/Layer/Filter/Chain/Link/Hidbulbs/Kelvin.php
+++ app/code/local/Soularpanic/CarToGraphEE/Model/Resource/Buyersguide/Layer/Filter/Chain/Link/Hidbulbs/Kelvin.php
@@ -9,50 +9,70 @@ class Soularpanic_CarToGraphEE_Model_Resource_Buyersguide_Layer_Filter_Chain_Lin
 
         $_dfBundleTarget = 'HID Bulbs';
         $dfBundleTarget = strtolower(str_replace(' ', '_', $_dfBundleTarget));
-        $carAlias = "car_$dfBundleTarget";
-        $linkAlias = "carlink_$dfBundleTarget";
-        $optionProductAlias = "option_product_$dfBundleTarget";
-        $fitOptionAlias = "fit_option_$dfBundleTarget";
-        $attributeSetAlias = "attribute_set_$dfBundleTarget";
+
         $f = "f_$dfBundleTarget";
-
-
-        //$chain = $filter->getChain();
         $state = $filter->getChainState();
-//        $carId = $state['car_id'];
 
-        $bulbAlias = 'f_hid_bulbs';
-//        $nextStepValue = Mage::app()->getRequest()->getParam($this->_NEXT_STEP_VAR_NAME);
-//        Mage::log("Next step value: ($nextStepValue)", null, 'trs_guide.log');
-//        if ($fitSkuArr) {
         if ($value) {
             Mage::log("Altering SQL...", null, 'trs_guide.log');
             $collection = $filter->getLayer()->getProductCollection();
             $directFitSelect = $collection->getSelect();
 
-            //$originalSelect = clone $directFitSelect;
-//            $fitSkus = "('".implode("','", $fitSkuArr)."')";
             $likeStr = str_replace('0', '_', str_replace('K', '%', $value)).'K%';
             $columnAlias = "preselect_$dfBundleTarget";
+
+            $catalogProductTable = $this->getTable('catalog/product_flat').'_'.Mage::app()->getStore()->getStoreId();
+
             $directFitSelect
                 ->join(['bulb_package_options' => $this->getTable('bundle/selection')],
                     "bulb_package_options.parent_product_id = e.entity_id",
                     [])
-                ->joinLeft([$f => $this->getTable('catalog/product_flat').'_'.Mage::app()->getStore()->getStoreId()],
+                ->joinLeft([$f => $catalogProductTable],
                     "$f.entity_id = bulb_package_options.product_id and $f.name like '$likeStr'",
                     [$columnAlias => "GROUP_CONCAT(DISTINCT $f.sku SEPARATOR ',')"])
-//                ->orWhere("$f.sku is not null")
                 ->where("$f.sku is not null")
                 ->having("$columnAlias is not null");
 
-            Mage::log("Shroud SQL:\n{$directFitSelect->__toString()}", null, 'trs_guide.log');
+
+            $crossreferenceTable = $filter->getCrossreference();
+            if ($crossreferenceTable) {
+                $crossRefRequestVar = array_keys($crossreferenceTable)[0];
+                $crossRefRequestValue = Mage::app()->getRequest()->getParam($crossRefRequestVar);
+                $crossRefSqlValue = $crossreferenceTable[$crossRefRequestVar][$crossRefRequestValue];
+
+                if ($crossRefSqlValue) {
+                    $subselect = Mage::getSingleton('core/resource')->getConnection('core_read')->select();
+                    $x = 'x';
+                    $y = 'y';
+                    $z = 'z';
+                    $subselect
+                        ->from([$x => $this->getTable('cartographee/linkcarproduct')],
+                            [ 'car_id' ])
+                        ->join([$y => $catalogProductTable],
+                            "$x.product_id = $y.entity_id",
+                            [ 'sku' ])
+                        ->join([$z => $this->getTable('eav/attribute_set')],
+                            "$y.attribute_set_id = $z.attribute_set_id",
+                            [])
+                        ->where("$z.attribute_set_name = '$_dfBundleTarget'")
+                        ->where("$x.type = '$crossRefSqlValue'");
+                    Mage::log("\n\nSubselect SQL:\n".$subselect->__toString(), null, 'trs_guide.log');
+
+                    $foo = "foo";
+                    $directFitSelect
+                        ->joinLeft([$foo => $subselect],
+                            "$foo.car_id = car.entity_id",
+                            [])
+                        ->where("if($foo.sku is not null, $f.sku in ($foo.sku), true)");
+                }
+            }
+
+            Mage::log("HID Bulbs Kelvin SQL:\n{$directFitSelect->__toString()}", null, 'trs_guide.log');
 
             $state['action'] = 'step:done';
             $filter->setChainState($state);
         }
 
-//        $state['action'] = $refineFurther ? $option->getAction() : 'step:done';
-//        $filter->setChainState($state);
 
 
         Mage::log("Request vars:\n".print_r(Mage::app()->getRequest()->getParams(), true), null, 'trs_guide.log');
